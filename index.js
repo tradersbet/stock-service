58569275dd82fef9e099f2ba95ced4b4e7f712be
alex-subtselnyi <subtselnyi@gmail.com>
2018-12-22 01:04:30 +0200

    parse Binance, push to redis and ws clients

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
+'use strict';
+
+const http = require('http');
+const Websocket = require('websocket').server;
+
+const redis = require('redis');
+const client = redis.createClient(); 
+
 const binance = require('node-binance-api')().options({
   APIKEY: "yTGrczsnJIxGnlRTkmHW2dMAAcPoHNUtoqC2SgT5yWKnv2UFjQ7YaM4zP5zO1z1Q",
   APISECRET: "CcpnnqTMCu4S5pAEFcHsEMcVwEPuKyoqqo4ySbTm2LzSHRMTysDbtZ8FFIse1t7Z",
-  useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
-  test: true // If you want to use sandbox mode where orders are simulated
+  useServerTime: true, 
 });
 
-const redis = require('redis');
-const client = redis.createClient(); // this creates a new client
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end(index);
+});
+
+server.listen(8000, () => {
+  console.log('Listen port 8000');
+});
+
+const ws = new Websocket({
+  httpServer: server,
+  autoAcceptConnections: false
+});
 
 client.on('connect', function() {
     console.log('Redis client connected');
 });
 
-client.set('my test key', 'my test value', redis.print);
-client.get('my test key', function (error, result) {
-    if (error) {
-        console.log(error);
-        throw error;
-    }
-    console.log('GET result ->' + result);
+const connections = [];
+
+ws.on('request', req => {
+  const connection = req.accept('', req.origin);
+  connections.push(connection);
+  console.log('Connected ' + connection.remoteAddress);
+  connection.on('message', message => {
+    const dataName = message.type + 'Data';
+    const data = message[dataName];
+    console.log('Received: ' + data);
+    connections.forEach(client => {
+      if (connection !== client) {
+        client.send(data);
+      }
+    });
+  });
+  connection.on('close', (reasonCode, description) => {
+    console.log('Disconnected ' + connection.remoteAddress);
+    console.dir({ reasonCode, description });
+  });
+});
+
+
+binance.websockets.miniTicker(markets => {
+  let prices = JSON.stringify(markets);
+  client.set('prices', prices, redis.print);
+  connections.forEach(client => {
+        client.send(prices);
+  });
 });
